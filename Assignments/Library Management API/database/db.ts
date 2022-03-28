516c9fa80642eb9f78e3c3b4777d5b33b690ab56
Prathmesh Gheware <72719825+prath1409@users.noreply.github.com>
2022-03-28 10:44:43 +0530

    Add files via upload

---
+++ Assignments/Library Management API/database/db.ts
@@ -0,0 +1,12 @@
+
+const Pool = require('pg').Pool;
+const Client = require('pg')
+
+
+export const pool: typeof Client = new Pool({
+    user: 'postgres',
+    password: '1SUL5T4G',
+    database: 'Library_Management_System',
+    host: 'localhost',
+    port: 5432
+});
